b26b526d494bb5a1a8c52f3550eec35d1424b6e3
Saad <uddinsaad4567@gmail.com>
2025-06-21 02:46:47 +0530

    Fix DriveServiceHelper imports: replace android.support.annotation with androidx.annotation

--- app/src/main/java/com/example/barta_a_messenger_app/DriveServiceHelper.java
+++ app/src/main/java/com/example/barta_a_messenger_app/DriveServiceHelper.java
@@ -9,8 +9,8 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.provider.OpenableColumns;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Pair;
 import android.widget.Toast;
 
@@ -41,98 +41,104 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 /**
- * A utility for performing read/write operations on Drive files via the REST API and opening a
- * file picker UI via Storage Access Framework.
+ * A utility for performing read/write operations on Drive files via the REST
+ * API and opening a file picker UI via Storage Access Framework.
  */
 public class DriveServiceHelper {
+
     private static final Log log = LogFactory.getLog(DriveServiceHelper.class);
     private final Executor mExecutor = Executors.newSingleThreadExecutor();
     private final Drive mDriveService;
     private final Context context;
 
-    public DriveServiceHelper(Drive driveService , Context context) {
+    public DriveServiceHelper(Drive driveService, Context context) {
         mDriveService = driveService;
         this.context = context;
     }
 
     /**
-     * Creates a text file in the user's My Drive folder and returns its file ID.
+     * Creates a text file in the user's My Drive folder and returns its file
+     * ID.
      */
     public Task<String> createFile() {
         return Tasks.call(mExecutor, () -> {
-                File metadata = new File()
-                        .setParents(Collections.singletonList("root"))
-                        .setMimeType("text/plain")
-                        .setName("Untitled file");
+            File metadata = new File()
+                    .setParents(Collections.singletonList("root"))
+                    .setMimeType("text/plain")
+                    .setName("Untitled file");
 
-                File googleFile = mDriveService.files().create(metadata).execute();
-                if (googleFile == null) {
-                    throw new IOException("Null result when requesting file creation.");
-                }
+            File googleFile = mDriveService.files().create(metadata).execute();
+            if (googleFile == null) {
+                throw new IOException("Null result when requesting file creation.");
+            }
 
-                return googleFile.getId();
-            });
+            return googleFile.getId();
+        });
     }
 
     /**
-     * Opens the file identified by {@code fileId} and returns a {@link Pair} of its name and
-     * contents.
+     * Opens the file identified by {@code fileId} and returns a {@link Pair} of
+     * its name and contents.
      */
     public Task<Pair<String, String>> readFile(String fileId) {
         return Tasks.call(mExecutor, () -> {
-                // Retrieve the metadata as a File object.
-                File metadata = mDriveService.files().get(fileId).execute();
-                String name = metadata.getName();
+            // Retrieve the metadata as a File object.
+            File metadata = mDriveService.files().get(fileId).execute();
+            String name = metadata.getName();
 
-                // Stream the file contents to a String.
-                try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream();
-                     BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
+            // Stream the file contents to a String.
+            try (InputStream is = mDriveService.files().get(fileId).executeMediaAsInputStream(); BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
 
-                    while ((line = reader.readLine()) != null) {
-                        stringBuilder.append(line);
-                    }
-                    String contents = stringBuilder.toString();
-
-                    return Pair.create(name, contents);
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
                 }
-            });
+                String contents = stringBuilder.toString();
+
+                return Pair.create(name, contents);
+            }
+        });
     }
 
     /**
-     * Updates the file identified by {@code fileId} with the given {@code name} and {@code
+     * Updates the file identified by {@code fileId} with the given {@code name}
+     * and {@code
      * content}.
      */
     public Task<Void> saveFile(String fileId, String name, String content) {
         return Tasks.call(mExecutor, () -> {
-                // Create a File containing any metadata changes.
-                File metadata = new File().setName(name);
+            // Create a File containing any metadata changes.
+            File metadata = new File().setName(name);
 
-                // Convert content to an AbstractInputStreamContent instance.
-                ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
+            // Convert content to an AbstractInputStreamContent instance.
+            ByteArrayContent contentStream = ByteArrayContent.fromString("text/plain", content);
 
-                // Update the metadata and contents.
-                mDriveService.files().update(fileId, metadata, contentStream).execute();
-                return null;
-            });
+            // Update the metadata and contents.
+            mDriveService.files().update(fileId, metadata, contentStream).execute();
+            return null;
+        });
     }
 
     /**
-     * Returns a {@link FileList} containing all the visible files in the user's My Drive.
+     * Returns a {@link FileList} containing all the visible files in the user's
+     * My Drive.
      *
-     * <p>The returned list will only contain files visible to this app, i.e. those which were
-     * created by this app. To perform operations on files not created by the app, the project must
-     * request Drive Full Scope in the <a href="https://play.google.com/apps/publish">Google
-     * Developer's Console</a> and be submitted to Google for verification.</p>
+     * <p>
+     * The returned list will only contain files visible to this app, i.e. those
+     * which were created by this app. To perform operations on files not
+     * created by the app, the project must request Drive Full Scope in the
+     * <a href="https://play.google.com/apps/publish">Google Developer's
+     * Console</a> and be submitted to Google for verification.</p>
      */
     public Task<FileList> queryFiles() {
-        return Tasks.call(mExecutor, () ->
-                mDriveService.files().list().setSpaces("drive").execute());
+        return Tasks.call(mExecutor, ()
+                -> mDriveService.files().list().setSpaces("drive").execute());
     }
 
     /**
-     * Returns an {@link Intent} for opening the Storage Access Framework file picker.
+     * Returns an {@link Intent} for opening the Storage Access Framework file
+     * picker.
      */
     public Intent createFilePickerIntent() {
         Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -143,38 +149,39 @@ public class DriveServiceHelper {
     }
 
     /**
-     * Opens the file at the {@code uri} returned by a Storage Access Framework {@link Intent}
-     * created by {@link #createFilePickerIntent()} using the given {@code contentResolver}.
+     * Opens the file at the {@code uri} returned by a Storage Access Framework
+     * {@link Intent} created by {@link #createFilePickerIntent()} using the
+     * given {@code contentResolver}.
      */
     public Task<Pair<String, String>> openFileUsingStorageAccessFramework(
             ContentResolver contentResolver, Uri uri) {
         return Tasks.call(mExecutor, () -> {
-                // Retrieve the document's display name from its metadata.
-                String name;
-                try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
-                    if (cursor != null && cursor.moveToFirst()) {
-                        int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
-                        name = cursor.getString(nameIndex);
-                    } else {
-                        throw new IOException("Empty cursor returned for file.");
-                    }
+            // Retrieve the document's display name from its metadata.
+            String name;
+            try (Cursor cursor = contentResolver.query(uri, null, null, null, null)) {
+                if (cursor != null && cursor.moveToFirst()) {
+                    int nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                    name = cursor.getString(nameIndex);
+                } else {
+                    throw new IOException("Empty cursor returned for file.");
                 }
+            }
 
-                // Read the document's contents as a String.
-                String content;
-                try (InputStream is = contentResolver.openInputStream(uri);
-                     BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        stringBuilder.append(line);
-                    }
-                    content = stringBuilder.toString();
+            // Read the document's contents as a String.
+            String content;
+            try (InputStream is = contentResolver.openInputStream(uri); BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
                 }
+                content = stringBuilder.toString();
+            }
 
-                return Pair.create(name, content);
-            });
+            return Pair.create(name, content);
+        });
     }
+
     public Task<String> createFileWithContent(String name, String mimeType, ByteArrayContent content) {
         return Tasks.call(mExecutor, () -> {
             File metadata = new File()
@@ -241,7 +248,6 @@ public class DriveServiceHelper {
 //                    .execute();
 //        });
 //    }
-
     public Task<File> uploadFile(Uri fileUri, String fileName) {
         return Tasks.call(Executors.newSingleThreadExecutor(), () -> {
             // Get the MIME type of the file
@@ -286,6 +292,5 @@ public class DriveServiceHelper {
             android.util.Log.d(TAG, "makeFilePublic:    Error setting file permission: " + e.getMessage());
         }
     }
-
 
 }
